4ee30f0f3adbf043611c2578c0d237d8ce5eb749
Vojtěch Kváš <kvasvojt@fit.cvut.cz>
2025-11-28 10:20:08 +0100

    feat: add settings

---
+++ src/config/settings.py
@@ -0,0 +1,18 @@
+# 1. File Paths
+RAW_TWEETS_PATH = "data/raw/musk_quote_tweets.csv"
+RAW_DOGE_PRICE_PATH = "data/raw/doge_price.csv"
+PROCESSED_DATA_PATH = "data/processed/combined_data.csv"
+OUTPUT_ANALYSIS_DIR = "data/output"
+
+# 2. Column Names and Keywords
+DOGE_KEYWORDS = ["doge", "dogecoin", "DOGE"]
+DATE_TIME_COLUMN = "date"
+
+# 3. Parameters for Analysis
+JOINING_WINDOW_HOURS = 2
+
+
+# 4. Visualization Settings
+DASH_APP_TITLE = "Elon Musk & Dogecoin Impact Analysis"
+SERVER_PORT = 8050
+PRIMARY_COLOR = "#FFA400"
