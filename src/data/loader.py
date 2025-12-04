b9442c5ca17826ca6d82e531940f47e89de8f370
Vojtěch Kváš <kvasvojt@fit.cvut.cz>
2025-12-04 22:30:19 +0100

    feat: loader, chage directory to array

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,15 +1,23 @@
 import pandas as pd
+from typing import List, Optional, Dict
 import os
+ 
+def load_data(directory: List[str], filename: str, separator: str = None , types: Optional[Dict[str, str]] = None, skiprows:int= 0) -> pd.DataFrame:
 
-
-def load_data(directory, filename):
-    # logging.info(f"Loading data from {os.path.join(directory, filename)}")
-    file_path = os.path.join(directory, filename)
+    file_path = os.path.join(*directory, filename)
 
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"The file {filename} does not exist in {directory}")
+    
+    kwargs = {}
+    if separator is not None:
+        kwargs["sep"] = separator
+    
+    if types is not None:
+        kwargs["names"] = types.keys()
+        kwargs["dtype"] = types 
 
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path,  **kwargs ,   skiprows=skiprows, low_memory=False)
 
     return df
 
